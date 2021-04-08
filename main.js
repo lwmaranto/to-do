83029f49b08b467ce9621e9f5b277b1f858dc732
lwmaranto <laurawmaranto@gmail.com>
2021-04-07 21:38:15 -0700

    setup

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-'use strict'
+"use strict";
 
-const {db} = require('./server/db')
-const app = require('./server')
-const PORT = 3000
+const { db } = require("./server/db");
+const app = require("./server");
+const PORT = 3000;
 
+const port = process.env.PORT || PORT; // this can be very useful if you deploy to Heroku!
 
-
-const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
-app.listen(port, function () {
-  console.log(`Your server, listening on port ${port}`);
-})
-
-/*db.sync() 
-  .then(() => {
-    //console.log('db synced')
-    app.listen(PORT, () => console.log(`Your server, listening on port  ${PORT}`))
-  })*/
+db.sync().then(() => {
+  //console.log('db synced')
+  app.listen(PORT, () =>
+    console.log(`Your server, listening on port  ${PORT}`)
+  );
+});
